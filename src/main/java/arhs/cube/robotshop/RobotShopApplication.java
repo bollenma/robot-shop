4b095f64b70880e53ea71260a091564bc5ea9f8e
bollenma <mathieu.bollen@arhs-cube.com>
2018-01-19 15:07:25 +0100

    Initial commit

---
+++ src/main/java/arhs/cube/robotshop/RobotShopApplication.java
@@ -0,0 +1,12 @@
+package arhs.cube.robotshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RobotShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RobotShopApplication.class, args);
+	}
+}
