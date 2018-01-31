25897ff61e162475f97ec22139f11bdef11b3f10
bollenma <mathieu.bollen@arhs-cube.com>
2018-01-31 17:27:11 +0100

    delete useless directive

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,16 +5,15 @@ import {BrowserModule} from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import {AppComponent} from './app.component';
+import {DialogRobotCreatedComponent} from './components/form-robot/dialogs/dialog-robot-created/dialog-robot-created.component';
+import {DialogRobotCreationErrorComponent} from './components/form-robot/dialogs/dialog-robot-creation-error/dialog-robot-creation-error.component';
 import {FormRobotComponent} from './components/form-robot/form-robot.component';
 import {PageRobotsListComponent} from './components/pages/page-robots-list/page-robots-list.component';
 import {PageRobotsNewComponent} from './components/pages/page-robots-new/page-robots-new.component';
-import {ActiveItemDirective} from './directives/active-item.directive';
 import {MaterialModule} from './modules/material.module';
 import {RoutingModule} from './modules/routing.module';
 import {RobotModelService} from './services/robot-model/robot-model.service';
 import {RobotService} from './services/robot/robot.service';
-import {DialogRobotCreatedComponent} from './components/form-robot/dialogs/dialog-robot-created/dialog-robot-created.component';
-import {DialogRobotCreationErrorComponent} from './components/form-robot/dialogs/dialog-robot-creation-error/dialog-robot-creation-error.component';
 
 @NgModule({
   declarations: [
@@ -22,7 +21,6 @@ import {DialogRobotCreationErrorComponent} from './components/form-robot/dialogs
     PageRobotsListComponent,
     PageRobotsNewComponent,
     FormRobotComponent,
-    ActiveItemDirective,
     DialogRobotCreatedComponent,
     DialogRobotCreationErrorComponent,
   ],
