8cd0592f4833b681825f8d3176a8911f49721351
bollenma <mathieu.bollen@arhs-cube.com>
2018-02-02 15:10:52 +0100

    https try

--- frontend/src/main/frontend/src/app/constants/ConstantsURL.ts
+++ frontend/src/main/frontend/src/app/constants/ConstantsURL.ts
@@ -1,4 +1,4 @@
-export const URL_SERVER = 'http://localhost:8080/';
+export const URL_SERVER = 'https://localhost:8080/';
 export const API_ROBOTS = 'api/robots';
 export const API_MODELS = 'api/models';
 
