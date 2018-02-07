7d06d8f21fcdb1331aac87f5f7eedad5f281bd9e
bollenma <mathieu.bollen@arhs-cube.com>
2018-02-07 15:34:04 +0100

    Use Lombok for the entities

--- backend/src/main/java/arhs/cube/robotshop/core/Robot.java
+++ backend/src/main/java/arhs/cube/robotshop/core/Robot.java
@@ -4,9 +4,7 @@ import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 
 import arhs.cube.robotshop.dto.RobotDto;
-import org.apache.commons.lang3.builder.EqualsBuilder;
-import org.apache.commons.lang3.builder.HashCodeBuilder;
-import org.apache.commons.lang3.builder.ToStringBuilder;
+import lombok.Data;
 
 /**
  * The Robot entity
@@ -14,6 +12,7 @@ import org.apache.commons.lang3.builder.ToStringBuilder;
  * @author bollenma
  */
 @Entity
+@Data
 public class Robot {
 
     @Id
@@ -36,105 +35,15 @@ public class Robot {
     @NotNull
     private boolean soldout;
 
-    public Robot(){
-
+    public Robot() {
     }
 
-    public Robot(final RobotDto dto, final RobotModel model){
+    public Robot(final RobotDto dto, final RobotModel model) {
         this.id = dto.getId();
         this.name = dto.getName();
         this.model = model;
         this.price = dto.getPrice();
         this.pictureHash = dto.getPictureHash();
         this.soldout = dto.isSoldout();
-    }
-
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(final Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(final String name) {
-        this.name = name;
-    }
-
-    public RobotModel getModel() {
-        return model;
-    }
-
-    public void setModel(final RobotModel model) {
-        this.model = model;
-    }
-
-    public Float getPrice() {
-        return price;
-    }
-
-    public void setPrice(final Float price) {
-        this.price = price;
-    }
-
-    public String getPictureHash() {
-        return pictureHash;
-    }
-
-    public void setPictureHash(final String pictureHash) {
-        this.pictureHash = pictureHash;
-    }
-
-    public boolean isSoldout() {
-        return soldout;
-    }
-
-    public void setSoldout(final boolean soldout) {
-        this.soldout = soldout;
-    }
-
-    @Override
-    public String toString() {
-        return new ToStringBuilder(this)
-                .append("id", id)
-                .append("name", name)
-                .append("model", model)
-                .append("price", price)
-                .toString();
-    }
-
-    @Override
-    public boolean equals(final Object o) {
-        if (this == o) return true;
-
-        if (o == null || getClass() != o.getClass()) return false;
-
-        final Robot robot = (Robot) o;
-
-        return new EqualsBuilder()
-                .append(isSoldout(), robot.isSoldout())
-                .append(getId(), robot.getId())
-                .append(getName(), robot.getName())
-                .append(getModel(), robot.getModel())
-                .append(getPrice(), robot.getPrice())
-                .append(getPictureHash(), robot.getPictureHash())
-                .isEquals();
-    }
-
-    @Override
-    public int hashCode() {
-        return new HashCodeBuilder(17, 37)
-                .append(getId())
-                .append(getName())
-                .append(getModel())
-                .append(getPrice())
-                .append(getPictureHash())
-                .append(isSoldout())
-                .toHashCode();
     }
 }
--- backend/src/main/java/arhs/cube/robotshop/core/RobotModel.java
+++ backend/src/main/java/arhs/cube/robotshop/core/RobotModel.java
@@ -6,6 +6,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
 
+import lombok.Data;
 import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
 import org.apache.commons.lang3.builder.ToStringBuilder;
@@ -14,6 +15,7 @@ import org.apache.commons.lang3.builder.ToStringBuilder;
  * @author bollenma
  */
 @Entity
+@Data
 public class RobotModel {
 
     @Id
@@ -26,58 +28,4 @@ public class RobotModel {
     @NotNull
     private String label;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(final Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(final String name) {
-        this.name = name;
-    }
-
-    public String getLabel() {
-        return label;
-    }
-
-    public void setLabel(final String label) {
-        this.label = label;
-    }
-
-    @Override
-    public String toString() {
-        return new ToStringBuilder(this)
-                .append("id", id)
-                .append("name", name)
-                .append("label", label)
-                .toString();
-    }
-
-    @Override
-    public boolean equals(final Object o) {
-        if (this == o) return true;
-
-        if (o == null || getClass() != o.getClass()) return false;
-
-        final RobotModel that = (RobotModel) o;
-
-        return new EqualsBuilder()
-                .append(getId(), that.getId())
-                .append(getName(), that.getName())
-                .isEquals();
-    }
-
-    @Override
-    public int hashCode() {
-        return new HashCodeBuilder(17, 37)
-                .append(getId())
-                .append(getName())
-                .toHashCode();
-    }
 }
